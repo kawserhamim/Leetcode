580fcf1d2fdd5812f2fb4e07758ea8293faac7e1
kawser hamim <mdkawserhamim91824@gmail.com>
2025-03-21 02:59:12 +0600

    Time: 103 ms (95.05%), Space: 76.4 MB (11.06%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    int atMostK(vector<int>& nums, int k) {
-        int left = 0, count = 0;
-        unordered_map<int, int> freq;
-
-        for (int right = 0; right < nums.size(); ++right) {
-            freq[nums[right]]++;
-            
-            while (freq.size() > k) {
-                freq[nums[left]]--;
-                if (freq[nums[left]] == 0)
-                    freq.erase(nums[left]);
-                left++;
-            }
-            
-            count += (right - left + 1);
-        }
-        
-        return count;
-    }
-    
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return atMostK(nums, k) - atMostK(nums, k - 1);
+        int n = nums.size();
+        int i = 0, j = 0 , ans = 0 , x = 0 , y = 0 ;
+      //  unordered_map<int,int>mp ;
+        unordered_map<int,set<int>>st;
+        while(i < n)
+        {
+           if(st[nums[i]].empty())x++;
+         //  mp[nums[i]]++;
+           st[nums[i]].insert(i);
+           if(x == k)y++;
+           if(x == k)
+           {
+              if(i + 1 >= n or st[nums[i+1]].empty())
+              {
+                int z = y ;
+                while(x == k)
+                {
+                   auto it = st[nums[j]].begin();
+                   st[nums[j]].erase(it);
+                   if(st[nums[j]].empty())x--;
+                   else{
+                     auto it = st[nums[j]].begin();
+                     ans +=min(z,i-(*it)+1);
+                     z = min(z,i-(*it)+1);
+                   }
+                 //  mp[nums[j]]--;
+                   j++;
+                }
+                ans += y ;
+                y = 0 ;
+              }
+           }
+           i++;
+        }   
+        return ans ;
     }
 };
