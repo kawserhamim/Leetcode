bb62e4613b1b9106219980ddfe75369b3cff40f9
kawser hamim <mdkawserhamim91824@gmail.com>
2025-08-26 06:34:56 +0600

    Time: 0 ms (100%), Space: 21.6 MB (92.45%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    int sum = 0 ;
+    for(auto it : nums)
+      sum += it ;
+      int n = nums.size();
+      n = (n * (n + 1)) / 2 ;
+      return abs(sum - n) ;    
+    }
+};
