7332211a0c67e1dec83f8db13b94f041a509ddc8
kawser hamim <mdkawserhamim91824@gmail.com>
2025-10-02 03:07:19 +0600

    Time: 6 ms (50.88%), Space: 9.8 MB (70.4%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string repeated = a;
+        int count = 1;
+
+        
+        while (repeated.size() < b.size()) {
+            repeated += a;
+            count++;
+        }
+
+        int x = 0 ;
+        if (repeated.find(b) != string::npos) return count;
+
+        
+        repeated += a;
+        if (repeated.find(b) != string::npos) return count + 1;
+
+        return -1;
+    }
+};
