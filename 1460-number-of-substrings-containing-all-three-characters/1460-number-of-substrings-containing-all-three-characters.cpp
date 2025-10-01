da742884e328fc44547e3f6b00d2f5c97f352865
kawser hamim <mdkawserhamim91824@gmail.com>
2025-10-01 18:29:05 +0600

    Time: 15 ms (35.97%), Space: 11 MB (57.25%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-    int n = s.size();
-    unordered_map<char,deque<int>>mp;
-      for(int i = 0 ; i < s.size() ; i++){
-         mp[s[i]].push_back(i);
-      }
-      if(mp['a'].size() == 0 || mp['b'].size() == 0 || mp['c'].size() == 0)
-      return 0 ;
-      int ans =  0;
-      int x = max(mp['a'][0],mp['b'][0]);
-      int y = mp['c'][0];
-      x = max(x , y);
-      ans += (n - x);
-      for (int i = 0; i < n ; i++)
-      {
-         char c = s[i];
-         mp[s[i]].pop_front();
-         if(!mp[c].size())
-         {
-            break;
-         }else{
-      x = max(mp['a'][0],mp['b'][0]);
-      y = mp['c'][0];
-      x = max(x , y);
-      ans += (n - x);
-         }
-      }
-      return ans ;     
+        int n = s.size();
+        vector<int> last(3, -1); // last index of a,b,c
+        int ans = 0;
+        
+        for (int i = 0; i < n; i++) {
+            last[s[i] - 'a'] = i;
+            if (last[0] != -1 && last[1] != -1 && last[2] != -1) {
+                ans += 1 + min({last[0], last[1], last[2]});
+            }
+        }
+        return ans;
     }
 };
