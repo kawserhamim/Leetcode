e0745be967a760c9873ccad3bfef41906984a90c
kawser hamim <mdkawserhamim91824@gmail.com>
2025-10-06 10:19:28 +0600

    Time: 0 ms (100%), Space: 26.1 MB (91.95%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,10 +6,10 @@ public:
         while (left < right) {
             int mid = left + (right - left) / 2;
 
-            if (mid % 2 == 1) mid--; // ensure mid is even
+            if (mid % 2 == 1) mid--; 
 
             if (nums[mid] == nums[mid + 1]) {
-                left = mid + 2;
+                left = mid + 2 ;
             } else {
                 right = mid - 1;
             }
