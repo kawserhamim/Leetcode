944b784ea0e039a8e8fc41fb9d0009c712f799eb
kawser hamim <mdkawserhamim91824@gmail.com>
2025-10-08 05:36:29 +0600

    Time: 0 ms (100%), Space: 9.8 MB (80.66%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
- void hamim(vector<int>nums,vector<vector<int>>&x,vector<int>y, int i)
-{
-  if(i==nums.size())
-  {
-    x.push_back(y);
-    return ;
-  }
-   hamim(nums,x,y,i+1);
-   long d = nums[i];
-   y.push_back(d);
-   hamim(nums,x,y,i+1);
-}
- vector<vector<int>> subsets(vector<int>& nums){
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int total = 1 << n; 
+        vector<vector<int>> result;
 
-    //vector<vector<long>>v;
-    int i = 0 ; vector<vector<int>> x ;
-     vector<int>y;
-     hamim(nums,x,y,i);
-     
-   return x ;
- }
+        for (int mask = 0; mask < total; mask++) {
+            vector<int> subset;
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) { 
+                    subset.push_back(nums[i]);
+                }
+            }
+            result.push_back(subset);
+        }
+
+        return result;
+    }
 };
